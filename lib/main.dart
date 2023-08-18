fa4cd1dd5301a333ecfededdd6ceb252e3d05efe
yahiyam~ <yahiyamuhammed00@gmail.com>
2023-08-19 03:40:31 +0530

    after first deploying

--- lib/main.dart
+++ lib/main.dart
@@ -68,27 +68,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-/* 
-app release note;
-step i;
-  1, make release branch 
-  2, add app logo to assets //https://romannurik.github.io/AndroidAssetStudio/icons-launcher
-  3, add in yaml file
-    dev_dependencies:
-      flutter_launcher_icons: ^0.13.1
-    flutter_icons:
-      android: true
-      ios: true
-      image_path: "assets/images/app_logo.png"
-  4, flutter clean && flutter pub get
-  5, flutter pub run flutter_launcher_icons:main
-  6, flutter pub global activate rename
-  7, flutter pub global run rename --appname "appname"
-  8, flutter pub global run rename --bundleId com.devname.appname  //here it's "com.yahiyamuhammed.usersapp"
-  9, run, test and push project.
-
-step ii;
-  1, 
-
-*/
